5056f3e3976a42294fbcf9794b6f321a279c993d
timdecillis <timdecillis@gmail.com>
2024-06-29 15:17:39 -0700

    partial implement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@ const fs = require("fs");
 const filepath =
   "/Users/theresebataclan/Desktop/Projects/scrape-it/files/basquiat-paintings.html";
 const html = fs.readFileSync(filepath, "utf8");
-const array = loadAndExtractFromHTML(html);
+let array = null;
+const load = async () => {
+  const data = await loadAndExtractFromHTML(html).then((data) => data);
+  array = data;
+};
+const mapped = array && array.map((artwork) => <div>{artwork.name}</div>);
 
 export default function Home() {
   return <div>Hello, dude</div>;
