7e084927023372998aa4c2a8d3a83e5ec6d0ff61
timdecillis <timdecillis@gmail.com>
2024-06-29 15:09:15 -0700

    added html

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { loadAndExtractFromHTML } from "../../server/scraper/index";
 const path = require("path");
+const fs = require("fs");
 
 const filepath = path.join(__dirname, "../files/basquiat-paintings");
+const html = fs.readFile(filepath, "utf8");
 
 export default function Home() {
   return <div>Hello, dude</div>;
