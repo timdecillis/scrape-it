60028c04d83ba1c0c66246e55516e6cc15751825
timdecillis <timdecillis@gmail.com>
2024-06-30 13:03:46 -0700

    removed unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,6 @@ import { loadAndExtractFromHTML } from "../../server/scraper/index";
 const path = require("path");
 const fs = require("fs");
 
-// export async function getStaticProps() {
-
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// }
-
-// export async function getStaticProps() {
-//   const html = fs.readFileSync(filepath, "utf8");
-//   const data = await loadAndExtractFromHTML(html);
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// }
-
-// const data = [{ name: "foo" }, { name: "bar" }, { name: "foobar" }];
-// const mapped = data.map((item, i) => <div key={i}>{item.name}</div>);
-
-// export default function Home({ data }: any) {
-//   return data.map((item: any, i: number) => {
-//     return <div key={i}>{item.name}</div>;
-//   });
-// }
-
 export default async function Home() {
   const filepath =
     "/Users/theresebataclan/Desktop/Projects/scrape-it/files/basquiat-paintings.html";
