4f6de407797ad6d7181d50ecf5ea6793e62aa5b1
timdecillis <timdecillis@gmail.com>
2024-06-30 12:42:57 -0700

    successfully fetched and rendered artworks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,6 @@ const path = require("path");
 const fs = require("fs");
 
 // export async function getStaticProps() {
-//   const filepath =
-//     "/Users/theresebataclan/Desktop/Projects/scrape-it/files/basquiat-paintings.html";
-//   const html = fs.readFileSync(filepath, "utf8");
-//   const data = await loadAndExtractFromHTML(html);
 
 //   return {
 //     props: {
@@ -15,9 +11,38 @@ const fs = require("fs");
 //   };
 // }
 
-const data = [{ name: "foo" }, { name: "bar" }];
-const mapped = data.map((item, i) => <div key={i}>{item.name}</div>);
+// export async function getStaticProps() {
+//   const html = fs.readFileSync(filepath, "utf8");
+//   const data = await loadAndExtractFromHTML(html);
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
 
-export default function Home() {
-  return mapped;
+// const data = [{ name: "foo" }, { name: "bar" }, { name: "foobar" }];
+// const mapped = data.map((item, i) => <div key={i}>{item.name}</div>);
+
+// export default function Home({ data }: any) {
+//   return data.map((item: any, i: number) => {
+//     return <div key={i}>{item.name}</div>;
+//   });
+// }
+
+export default async function Home() {
+  const filepath =
+    "/Users/theresebataclan/Desktop/Projects/scrape-it/files/basquiat-paintings.html";
+  const html = fs.readFileSync(filepath, "utf8");
+  const data = await loadAndExtractFromHTML(html);
+
+  return (
+    <div>
+      {data ? (
+        data.map((artwork, i) => <div key={i}>{artwork.name}</div>)
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
 }
