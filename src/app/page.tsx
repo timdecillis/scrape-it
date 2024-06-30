afd5356b4e27a80c1e68f9322f883c1641ecb3dd
timdecillis <timdecillis@gmail.com>
2024-06-30 12:22:50 -0700

    implemented mapped with dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,22 @@ import { loadAndExtractFromHTML } from "../../server/scraper/index";
 const path = require("path");
 const fs = require("fs");
 
-const filepath =
-  "/Users/theresebataclan/Desktop/Projects/scrape-it/files/basquiat-paintings.html";
-const html = fs.readFileSync(filepath, "utf8");
-let array = null;
-const load = async () => {
-  const data = await loadAndExtractFromHTML(html).then((data) => data);
-  array = data;
-};
-const mapped = array && array.map((artwork) => <div>{artwork.name}</div>);
+// export async function getStaticProps() {
+//   const filepath =
+//     "/Users/theresebataclan/Desktop/Projects/scrape-it/files/basquiat-paintings.html";
+//   const html = fs.readFileSync(filepath, "utf8");
+//   const data = await loadAndExtractFromHTML(html);
+
+//   return {
+//     props: {
+//       data,
+//     },
+//   };
+// }
+
+const data = [{ name: "foo" }, { name: "bar" }];
+const mapped = data.map((item, i) => <div key={i}>{item.name}</div>);
 
 export default function Home() {
-  return <div>Hello, dude</div>;
+  return mapped;
 }
