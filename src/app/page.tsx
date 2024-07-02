b5f75fb0e56a7d2b51025148146f0e6296caa632
timdecillis <timdecillis@gmail.com>
2024-07-02 12:38:22 -0700

    removed mapped art

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,11 @@ export default async function Home() {
   const [term, setTerm] = useState("");
   const [artworks, setArtworks] = useState([]);
 
-  const filepath = path.join(process.cwd(), `files/${term}-paintings.html`);
-  const html = fs.readFileSync(filepath, "utf8");
-  const data = await loadAndExtractFromHTML(html);
-
-  const handleSubmit = (e: SyntheticEvent) => {
+  const handleSubmit = async (e: SyntheticEvent) => {
     e.preventDefault();
+    const filepath = path.join(process.cwd(), `files/${term}-paintings.html`);
+    const html = fs.readFileSync(filepath, "utf8");
+    const data = await loadAndExtractFromHTML(html);
     const inputElement = e.target as HTMLInputElement;
     setTerm(inputElement.value);
   };
@@ -23,26 +22,6 @@ export default async function Home() {
         <input type="text" />
         <input type="submit" />
       </form>
-      {artworks.length ? (
-        artworks.map((artwork, i) => {
-          let year;
-          if (artwork.extensions) {
-            year = artwork.extensions[0];
-          }
-          return (
-            <>
-              <div key={i}>
-                {i + 1}.) {artwork.name}
-                {year && `, ${year}`}
-              </div>
-              <div>link: {artwork.link.slice(-5)}</div>
-              <div>{artwork.image && `image: ${artwork.image.slice(-5)}`}</div>
-            </>
-          );
-        })
-      ) : (
-        <p>Loading...</p>
-      )}
     </div>
   );
 }
