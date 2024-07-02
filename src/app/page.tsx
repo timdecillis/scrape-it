f03c4b5f4a88bf02c64a74b09dd2dac54831f92b
timdecillis <timdecillis@gmail.com>
2024-07-02 12:34:40 -0700

    added onSubmit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" />
         <input type="submit" />
       </form>
