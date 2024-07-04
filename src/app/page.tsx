8ecb658d264677164aa3b7a81d7fbf6a776a2317
timdecillis <timdecillis@gmail.com>
2024-07-04 13:57:53 -0700

    created async home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,42 +11,37 @@ interface Artwork {
   image: string;
 }
 
-export default function Home() {
-  const handleSubmit = async (e: SyntheticEvent) => {
-    e.preventDefault();
-    const inputElement = e.target as HTMLInputElement;
+export default async function Home() {
+  // const handleSubmit = async (e: SyntheticEvent) => {
+  //   e.preventDefault();
+  //   const inputElement = e.target as HTMLInputElement;
 
-    const data = await loadAndExtractFromHTML(inputElement);
-    console.log("data:", data);
-  };
+  //   const data = await loadAndExtractFromHTML(inputElement);
+  //   console.log("data:", data);
+  // };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" />
-        <input type="submit" />
-      </form>
+      {artworks.length ? (
+        artworks.map((artwork, i) => {
+          let year;
+          if (artwork.extensions) {
+            year = artwork.extensions[0];
+          }
+          return (
+            <>
+              <div key={i}>
+                {i + 1}.) {artwork.name}
+                {year && `, ${year}`}
+              </div>
+              <div>link: {artwork.link.slice(-5)}</div>
+              <div>{artwork.image && `image: ${artwork.image.slice(-5)}`}</div>
+            </>
+          );
+        })
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
-
-// {artworks.length ? (
-//   artworks.map((artwork, i) => {
-//     let year;
-//     if (artwork.extensions) {
-//       year = artwork.extensions[0];
-//     }
-//     return (
-//       <>
-//         <div key={i}>
-//           {i + 1}.) {artwork.name}
-//           {year && `, ${year}`}
-//         </div>
-//         <div>link: {artwork.link.slice(-5)}</div>
-//         <div>{artwork.image && `image: ${artwork.image.slice(-5)}`}</div>
-//       </>
-//     );
-//   })
-// ) : (
-//   <p>Loading...</p>
-// )}
